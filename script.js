6fb84d357fc4c86d1400e1cb0f1be5215252d9af
najahannah <206175225+najahannah@users.noreply.github.com>
2025-05-15 21:24:31 -0400

    edit playRound function, add playGame function

--- script.js
+++ script.js
@@ -5,37 +5,46 @@ function getComputerChoice() {
      let x = Math.floor(Math.random() * 3);
     
      if (x < 1) {
-     return "rock";
+        return "rock";
      } else if (x > 1) {
-     return "scissors";
+        return "scissors";
      } else {
-     return "paper";
+        return "paper";
      }
 }
 // console.log(getComputerChoice())
 
 function getHumanChoice() {
     let choice = prompt("Rock, paper, scissors...shoot!");
-    return choice.toLowerCase();
+        return choice.toLowerCase();
     }
 // console.log(getHumanChoice())
-    
-function playRound() {
 
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
+    
+function playRound(humanChoice, computerChoice) {
+
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
     
     if (humanChoice == computerChoice) {
         console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. This round is a tie!`);
     } else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper"){
         humanScore++;
         console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You win this round! Human: ${humanScore}, Computer: ${computerScore}`);
-        } else {
+    } else {
         computerScore++;
         console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. Computer wins this round! Human: ${humanScore}, Computer: ${computerScore}`);
     }
-    }
+}
 playRound()
+
+
+// function playGame() {
+
+
+   
+// }
+
 
     
 
